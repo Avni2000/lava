3ebe21da0182658645a726749fd94dd8ef64fa83
Avni2000 <avnibadiwale@gmail.com>
2026-06-14 01:42:27 -0500

    init

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import { LocationProvider, Router, Route, hydrate, prerender as ssr } from 'preact-iso';
+
+import { Header } from './components/Header.jsx';
+import { Home } from './pages/Home/index.jsx';
+import { NotFound } from './pages/_404.jsx';
+import './style.css';
+
+export function App() {
+	return (
+		<LocationProvider>
+			<Header />
+			<main>
+				<Router>
+					<Route path="/" component={Home} />
+					<Route default component={NotFound} />
+				</Router>
+			</main> 
+		</LocationProvider>
+	);
+}
+
+if (typeof window !== 'undefined') {
+	hydrate(<App />, document.getElementById('app') as HTMLElement);
+}
+
+/**
+ * This is for Static Site Generation (SSG) -- it allows the site to load with HTML without waiting for JS to load.
+ * @param data Data passed from the build tool (e.g., current URL)
+ */
+export async function prerender(data: any) {
+	return await ssr(<App {...data} />);
+}

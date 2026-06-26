28c1019079bb8c77576d575ff6747e998a33a59b
Avni2000 <avnibadiwale@gmail.com>
2026-06-25 23:10:16 -0500

    Messing with UI

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,40 +1,65 @@
 import "./style.css";
 import { useState, useEffect } from "preact/hooks";
 import { getAllPosts, Post } from "../../utils/getPost";
-import { Button, Card, Table } from "react-bootstrap";
+import { Button, Card, Table, Container, Row, Col } from "react-bootstrap";
 
 export function Home() {
-  const [posts, setPosts] = useState<Post[] | null>([]);
+	const [posts, setPosts] = useState<Post[] | null>([]);
 
-  useEffect(() => {
-    getAllPosts()
-      .then((p) => {
-        setPosts(p);
-        return p; // Pass the data to the next .then
-      })
-      .then(console.log);
-  }, []);
-  return (
-    <>
-      <div class="home">
-        <h1>~Avni/</h1>
-      </div>
-      <div class="posts">
-        {posts?.map((post) => (
-          <Card
-            key={post.title}
-            onClick={() => openPage(post)}
-            style={{ cursor: "pointer" }}
-          >
-            <Card.Body>
-              <Card.Title>{post.title}</Card.Title>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
-    </>
-  );
+	useEffect(() => {
+		getAllPosts()
+			.then((p) => {
+				setPosts(p);
+				return p; // Pass the data to the next .then
+			})
+			.then(console.log);
+	}, []);
+	return (
+		<>
+			<header className="header">
+				<h1>/home/avni/</h1>
+				<p style={{ textAlign: "left" }} className="home-path">
+					~/
+				</p>
+			</header>
+			<div>
+				<h2>about</h2>
+			</div>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+				}}
+			>
+				<h2>blog</h2>
+			</div>
+			<Container>
+				<Row>
+					<div class="posts">
+						{posts?.map((post) => (
+							<Card
+								key={post.title}
+								onClick={() => openPage(post)}
+								style={{
+									className: "card",
+									cursor: "pointer",
+								}}
+							>
+								<Card.Body>
+									<Card.Title>{post.title}</Card.Title>
+									<p style={{ fontSize: "15px" }}>
+										05/05/2025
+									</p>
+								</Card.Body>
+							</Card>
+						))}
+					</div>
+				</Row>
+			</Container>
+		</>
+	);
 }
 function openPage(post: Post) {
-  window.location.href = `/content/${post.filepath}`;
+	window.location.href = `/content/${post.filepath}`;
 }
